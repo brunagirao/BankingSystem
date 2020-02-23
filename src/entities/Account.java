e8677c321342a6d9fa2a44e888eb0d780551216d
Bruna Girão <brunagirao.s@gmail.com>
2020-02-23 15:16:25 -0300

    Withdrawal and deposit methods created in the Account class.

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -5,5 +5,49 @@ public class Account {
 	private int number;
 	private String holder;
 	private double balance;
+	
+	//USED WHEN THE CUSTOMER WANTS TO MAKE AN INITIAL DEPOSIT.
+	public Account(int number, String holder, double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	//USED WHEN THE CUSTOMER DOES NOT WISH TO MAKE AN INITIAL DEPOSIT.
+	public Account(int number, String holder) {
+		this.number = number;
+		this.holder = holder;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}	
+
+	public int getNumber() {
+		return number;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(double amount) {
+		
+		this.balance += amount;
+		
+	}
+	
+	public void withdraw (double amount) {
+		
+		this.balance -= (amount + 5.0);
+		
+	}
+	
+	
+	
 
 }
