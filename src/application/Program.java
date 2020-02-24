e55a0a7f426935349c9dfcf511e3cd0c209b5166
Bruna Girão <brunagirao.s@gmail.com>
2020-02-24 17:50:49 -0300

    Account creation logic added.

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,12 +3,15 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Account;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		Account account;
 		
 		System.out.print("Enter account number: ");
 		int number = sc.nextInt();
@@ -19,6 +22,20 @@ public class Program {
 
 		System.out.print("Is there na initial deposit? [y/n]");
 		char response = sc.next().charAt(0);
+		
+		if(response == 'y') {
+			
+			System.out.print("Enter initial deposit value: ");
+			double initialDeposit = sc.nextDouble();
+			
+			account = new Account(number, holder, initialDeposit);
+			
+		} else {
+			
+			account = new Account(number, holder);
+		}
+		
+		
 		
 		
 		
